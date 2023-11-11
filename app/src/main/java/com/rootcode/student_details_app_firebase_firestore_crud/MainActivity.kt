5aaed831ba0d0801ff18256ff44440da3843fe03
Irfan-Hossain <irfanhpssain@gmail.com>
2023-11-11 11:32:49 +0600

    start

---
+++ app/src/main/java/com/rootcode/student_details_app_firebase_firestore_crud/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rootcode.student_details_app_firebase_firestore_crud
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
